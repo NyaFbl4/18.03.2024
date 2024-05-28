181be2a4df2cffe0986551b76c5d1b4ad5fedb82
NyaFbl4 <nikitaizmaylov1402@gmail.com>
2024-05-28 14:27:44 +0300

    no message

---
+++ Assets/Scripts/GameManager/GameListener.cs
@@ -0,0 +1,26 @@
+﻿namespace ShootEmUp
+{
+    public interface IGameListener
+    {
+    }
+    
+    public interface IGameStartListener
+    {
+        void OnStartGame();
+    }
+    
+    public interface IGameFinishListener
+    {
+        void OnFinishGame();
+    } 
+    
+    public interface IGamePauseListener
+    {
+        void OnPauseGame();
+    }
+    
+    public interface IGameResumeListener
+    {
+        void OnResumeGame();
+    }
+}
---
+++ Assets/Scripts/GameManager/GameSystem.cs
@@ -0,0 +1,7 @@
+﻿namespace ShootEmUp
+{
+    public class GameSystem
+    {
+        
+    }
+}
